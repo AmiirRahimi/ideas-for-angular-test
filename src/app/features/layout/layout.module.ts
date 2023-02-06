ffe57612513bc67f927fefa28b1ddc28905f2122
amirrahimi <amirrahimi7162@gmail.com>
2023-02-06 17:03:29 +0330

    fix: config layout component and install agular material

--- src/app/features/layout/layout.module.ts
+++ src/app/features/layout/layout.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutPageComponent } from './page/layout-page.component';
 import { LayoutPresenterHeaderComponent } from './presenters/layout-presenter-header/layout-presenter-header.component';
+import { LayoutPageModule } from './page/layout-page.module';
+import { LayoutRouterModule } from './layout-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    LayoutPageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutPageModule,
+    LayoutRouterModule
   ]
 })
 export class LayoutModule { }
--- src/app/features/layout/page/layout-page.module.ts
+++ src/app/features/layout/page/layout-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutPageComponent } from './layout-page.component';
 import { LayoutContainersModule } from '../containers/layout-containers.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,8 @@ import { LayoutContainersModule } from '../containers/layout-containers.module';
   ],
   imports: [
     CommonModule,
-    LayoutContainersModule
+    LayoutContainersModule,
+    RouterModule,
   ]
 })
 export class LayoutPageModule { }
